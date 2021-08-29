a238d10ad5fb7db4fcc02dc1e7ffb377c398ebd1
jihad <82382055+JK-Developpeur@users.noreply.github.com>
2021-08-29 18:41:06 +0000

    Create-Prodact

--- JkShop/JkShop/Areas/Admin/Controllers/ProductsController.cs
+++ JkShop/JkShop/Areas/Admin/Controllers/ProductsController.cs
@@ -1,8 +1,13 @@
 ﻿using JkShop.Data;
+using JkShop.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,14 +17,60 @@ namespace JkShop.Areas.Admin.Controllers
     public class ProductsController : Controller
     {
         private ApplicationDbContext _db;
+        private IHostingEnvironment _he;
 
-        public ProductsController(ApplicationDbContext db)
+
+        public ProductsController(ApplicationDbContext db, IHostingEnvironment he)
         {
             _db = db;
+            _he = he;
+
         }
         public IActionResult Index()
         {
             return View(_db.Products.Include(c => c.Categories).Include(f => f.Tag).ToList());
+        }
+
+        //Get Create method
+        public IActionResult Create()
+        {
+            ViewData["CategoriesId"] = new SelectList(_db.Categories.ToList(), "Id", "Category");
+            ViewData["TagId"] = new SelectList(_db.Tags.ToList(), "Id", "Name");
+            return View();
+        }
+
+        //Post Create method
+        [HttpPost]
+        public async Task<IActionResult> Create(Products product, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                var searchProduct = _db.Products.FirstOrDefault(c => c.Name == product.Name);
+                if (searchProduct != null)
+                {
+                    ViewBag.message = "This product is already exist";
+                    ViewData["productTypeId"] = new SelectList(_db.Categories.ToList(), "Id", "ProductType");
+                    ViewData["TagId"] = new SelectList(_db.Tags.ToList(), "Id", "Name");
+                    return View(product);
+                }
+
+                if (image != null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    product.Image = "Images/" + image.FileName;
+                }
+
+                if (image == null)
+                {
+                    product.Image = "Images/noimage.PNG";
+                }
+                _db.Products.Add(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
         }
     }
 }
